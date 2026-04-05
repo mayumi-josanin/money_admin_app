1b89e8f7a721a297b7b6b63bd892e599840f1abc
Kenmochi Hiroki <e225408@ie.u-ryukyu.ac.jp>
2026-04-05 16:44:42 +0900

    Configure Supabase sync

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.SUPABASE_SYNC_CONFIG = {
-  enabled: false,
-  url: "",
-  anonKey: "",
-  redirectUrl: ""
+  enabled: true,
+  url: "https://eyefmyinebdtjshpsabd.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5ZWZteWluZWJkdGpzaHBzYWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNzAwMzIsImV4cCI6MjA5MDk0NjAzMn0.R7CcnFtbk4asChkuLz2YIr1kwtERw6i9PXbgFQadopg",
+  redirectUrl: "https://mayumi-josanin.github.io/money_admin_app/"
 };
